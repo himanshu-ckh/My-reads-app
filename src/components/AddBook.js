cd5c7f8bc2bbeaafc195c039b51989cf40d8c5ca
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-11 22:26:20 +0530

    Link to add Books

---
+++ src/components/AddBook.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function AddBook(){
+	return(
+			<div className="open-search">
+				<Link to="/search">
+				</Link>
+			</div>
+		)
+}
+export default AddBook
