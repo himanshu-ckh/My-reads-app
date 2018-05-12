7794ebbff2f79eaa301f73f8926274f5725f39c4
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-12 21:34:30 +0530

    Search included

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,44 +2,38 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 import Title from './Title'
 import ListBooks from './ListBooks'
+import AddBooks from './AddBooks'
+import Searchbooks from './SearchBooks'
 import * as BooksAPI from '../BooksAPI'
 
 class Main extends Component {
 	state = {
 		books: []
 	}
+
 	componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
     })
   }
+
   updateBook = (book, shelf) => {
-
-    book.shelf = shelf
-
-    if (this.state.books.filter((b) => b.id === book.id).length > 0) {
-      this.setState((state) => ({
-        books: state.books.map((b) => {
-          if (b.id === book.id) {
-            b = book
-          }
-          return b
-        })
-      }))
-    } else {
-      this.setState((state) => ({
-        books: state.books.concat([book])
-      }))
-    }
-
-    BooksAPI.update({id: book.id}, shelf)
-
+    let newShelfBook = this.state.books.map((b) => {
+      if (b.id === book.id) {
+        b.shelf = shelf
+      }
+      return b
+    })
+    this.setState( {books: newShelfBook} )
+    BooksAPI.update(book, shelf)
   }
+
 	render() {
 		return (
 			<div className="main-page">
 				<Title />
 				<ListBooks books={this.state.books} updateBook={this.updateBook} />
+				<AddBooks />
 			</div>
 	)
 }
