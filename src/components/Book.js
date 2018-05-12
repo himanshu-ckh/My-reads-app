73f96f97ee22047233b8abda74121eb78c52e387
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-12 21:33:00 +0530

    Option and deffault value changed

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,51 +1,26 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 function Book (props) {
   const {book} = props
-
   return (
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
         </div>
         <div className="book-shelf-changer">
-          <select
-            onChange={(event) => {
-              props.updateBook(book, event.target.value)
-            }
-            }
-            defaultValue={book.shelf}
-          >
+          <select defaultValue={book.shelf} onChange={(e) => {props.updateBook(book, e.target.value)}}>
             <option value="none" disabled>Move to...</option>
-            <option
-              value="currentlyReading"
-            >Currently Reading
-            </option>
-            <option
-              value="wantToRead"
-            >Want to Read
-            </option>
-            <option
-              value="read"
-            >Read
-            </option>
-            <option
-              value="none"
-            >None
-            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None </option>
           </select>
         </div>
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors && (book.authors.join(', '))}</div>
+      {book.authors && (book.authors.join(', '))}
     </div>
   )
-}
-
-Book.propTypes = {
-  book: PropTypes.object.isRequired,
-  updateBook: PropTypes.func.isRequired
 }
 
 export default Book
