7ade5826013072722be4e42ead160571a84c03eb
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-14 18:56:06 +0530

    Spelling mistake

--- src/components/Book.js
+++ src/components/Book.js
@@ -18,7 +18,7 @@ function Book (props) {
         </div>
         </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authores">{book.authors && (book.authors.join(', '))} </div>
+      <div className="book-authors">{book.authors && (book.authors.join(', '))} </div>
     </div>
   )
 }
